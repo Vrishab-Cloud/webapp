1ffdf3653c4d43c36fdc8ef02a8d14229f80d90e
vrishab-shetty <shetty.vr@northeastern.edu>
2024-02-10 23:52:18 -0500

    Updated the github workflow and tests.
    Test: Changed to test the database.
    Github workflow: Setup datbase and env file for test case.

--- tests/database.test.js
+++ tests/database.test.js
@@ -1,41 +1,41 @@
-// const { v4: uuidv4 } = require("uuid");
-// const { db } = require("../models");
+ const { v4: uuidv4 } = require("uuid");
+ const { db } = require("../models");
 
-// const id = uuidv4();
+ const id = uuidv4();
 
-// beforeAll(async () => {
-//   await db.checkDatabaseCreation();
-//   await db.sequelize.sync({ force: true });
+ beforeAll(async () => {
+   await db.checkDatabaseCreation();
+   await db.sequelize.sync({ force: true });
+ });
+
+ test("create user", async () => {
+   expect.assertions(1);
+
+   const user = await db.User.create({
+      id: id,
+      firstName: "James",
+      lastName: "Bond",
+      username: "jamebond@domain.name.com",
+      password: "Im the best",
+      account_created: new Date(),
+      account_updated: new Date(),
+    });
+
+    expect(user.id).toEqual(id);
+  });
+
+  test("get user", async () => {
+    expect.assertions(2);
+    const user = await db.User.findByPk(id);
+    expect(user.firstName).toEqual("James");
+    expect(user.lastName).toEqual("Bond");
+  });
+
+  afterAll(async () => {
+    await db.sequelize.close();
+  });
+
+// test("sample test", async () => {
+//  expect.assertions(1);
+//  expect(1 + 1).toBe(3);
 // });
-
-// test("create user", async () => {
-//   expect.assertions(1);
-
-//   const user = await db.User.create({
-//     id: id,
-//     firstName: "James",
-//     lastName: "Bond",
-//     username: "jamebond@domain.name.com",
-//     password: "Im the best",
-//     account_created: new Date(),
-//     account_updated: new Date(),
-//   });
-
-//   expect(user.id).toEqual(id);
-// });
-
-// test("get user", async () => {
-//   expect.assertions(2);
-//   const user = await db.User.findByPk(id);
-//   expect(user.firstName).toEqual("James");
-//   expect(user.lastName).toEqual("Bond");
-// });
-
-// afterAll(async () => {
-//   await db.sequelize.close();
-// });
-
-test("sample test", async () => {
-  expect.assertions(1);
-  expect(1 + 1).toBe(3);
-});
