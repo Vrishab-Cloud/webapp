dddc77974a020bc96bb11c295ec129f37d2c31c3
Vrishab Shetty <shetty.vr@northeastern.edu>
2024-02-25 15:06:27 -0500

    Remove the local mysql server

--- packer/scripts/updall.sh
+++ packer/scripts/updall.sh
@@ -5,11 +5,5 @@ sudo dnf update rpm -y
 sudo dnf update -y
 
 # Install libraries
-sudo dnf install mysql-server -y
 sudo dnf module enable nodejs:20 -y
 sudo dnf install nodejs -y
-
-# Start MySQL
-sudo systemctl daemon-reload
-sudo systemctl enable mysqld
-sudo systemctl start mysqld
