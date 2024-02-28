9a4627496f57119c8f540b72e4f71d273524f7e6
Vrishab Shetty <shetty.vr@northeastern.edu>
2024-02-28 17:15:48 -0500

    Change the config file
    Now, Host depends on .env file

--- app/config/config.js
+++ app/config/config.js
@@ -21,7 +21,7 @@ module.exports = {
     username: process.env.PROD_DB_USER,
     password: process.env.PROD_DB_PASS,
     database: process.env.PROD_DB_NAME,
-    host: "127.0.0.1",
+    host: process.env.PROD_HOST,
     dialect: "mysql",
     port: "3306",
   },
