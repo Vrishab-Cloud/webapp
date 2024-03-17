a8a660029ccf40acdf36c0162732b13b953a82c1
vrishab <vrishabshetty@gmail.com>
2024-03-17 17:28:49 -0400

    Add script to install Ops Agent

---
+++ packer/scripts/ops-agent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo dnf install curl -y
+
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
