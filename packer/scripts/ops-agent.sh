c24feb49e64b9805eae1660120b1c5f909f3aec4
vrishab <vrishabshetty@gmail.com>
2024-03-18 01:49:36 -0400

    Configure the Ops Agent

--- packer/scripts/ops-agent.sh
+++ packer/scripts/ops-agent.sh
@@ -5,3 +5,9 @@ sudo dnf install curl -y
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+
+# Create log file and assign permission
+sudo mkdir /var/log/webapp 
+sudo touch /var/log/webapp/app.log
+sudo chown csye6225:csye6225 /var/log/webapp/app.log
