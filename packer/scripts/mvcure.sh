4b75a527ea1ac186068f9d037b4dd979988d6c33
vrishab <vrishabshetty@gmail.com>
2024-07-17 14:43:17 -0400

    Upgrade the code for centos 9 image

--- packer/scripts/mvcure.sh
+++ packer/scripts/mvcure.sh
@@ -5,6 +5,7 @@ sudo mv /tmp/app /opt/webapp
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
 
 sudo chown -R csye6225:csye6225 /opt/webapp/app
+sudo restorecon /etc/systemd/system/webapp.service
 
 # Setup webapp service
 sudo systemctl daemon-reload
